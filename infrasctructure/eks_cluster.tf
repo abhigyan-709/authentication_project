ea73bd657b7d38e94fb784e7ffb18e1653bd8351
abhigyan-709 <abhigkumar709@gmail.com>
2024-12-02 13:22:59 +0530

    Feature branch Initiated

--- infrasctructure/eks_cluster.tf
+++ infrasctructure/eks_cluster.tf
@@ -1,3 +1,9 @@
+# ECR Repository
+resource "aws_ecr_repository" "my_eks_repo" {
+  name = "my-python-app-repo"
+}
+
+# IAM Role for EKS Cluster
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
 
@@ -20,6 +26,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+# IAM Role for EKS Node Group
 resource "aws_iam_role" "eks_node_group_role" {
   name = "eks-node-group-role"
 
@@ -47,18 +54,68 @@ resource "aws_iam_role_policy_attachment" "eks_node_group_attach_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "eks_node_group_cni_policy" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# VPC Setup
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+# Subnets for EKS
+resource "aws_subnet" "eks_subnet_a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "EKS Subnet A"
+  }
+}
+
+resource "aws_subnet" "eks_subnet_b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "EKS Subnet B"
+  }
+}
+
+# Security Group for EKS
+resource "aws_security_group" "eks_security_group" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+# EKS Cluster Setup
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "my-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.eks_subnet_a.id, aws_subnet.eks_subnet_b.id]
+    subnet_ids              = [aws_subnet.eks_subnet_a.id, aws_subnet.eks_subnet_b.id]
+    endpoint_public_access  = true
+    endpoint_private_access = false
+    public_access_cidrs     = ["0.0.0.0/0"]
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_role_policy]
 }
 
-
+# EKS Node Group
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "my-node-group"
@@ -66,15 +123,30 @@ resource "aws_eks_node_group" "eks_node_group" {
   subnet_ids      = [aws_subnet.eks_subnet_a.id] # Ensure this is a list
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 1
+    max_size     = 1
     min_size     = 1
   }
 
   depends_on = [aws_eks_cluster.eks_cluster]
 }
 
+# Route Tables for Subnets
+resource "aws_route_table" "eks_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
 
+resource "aws_route_table_association" "eks_route_table_association_a" {
+  route_table_id = aws_route_table.eks_route_table.id
+  subnet_id      = aws_subnet.eks_subnet_a.id
+}
+
+resource "aws_route_table_association" "eks_route_table_association_b" {
+  route_table_id = aws_route_table.eks_route_table.id
+  subnet_id      = aws_subnet.eks_subnet_b.id
+}
+
+# Kubernetes Provider
 provider "kubernetes" {
   host                   = aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
